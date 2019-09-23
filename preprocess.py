a3e46473dc05009029a5cccf28e9e0803904d374
Y. Violet Guo <yueguo598@gmail.com>
2019-09-23 10:06:45 -0400

    manual running exps

--- preprocess.py
+++ preprocess.py
@@ -16,6 +16,8 @@ To preprocess the following:
 
 # data preprocessing imports
 
+import nltk
+nltk.download('wordnet')
 from sklearn.model_selection import train_test_split
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
@@ -96,13 +98,6 @@ class Unigram:
         }
         return params
 
-    def lemmatize(self, x):
-        pass
-
-    @classmethod
-    def stem(self, corporus):
-        return CountVectorizer(ngram_range=(1, 1))
-
     @classmethod
     def rm_stopwords(self):
         # TODO param stop words, english or none
@@ -124,18 +119,25 @@ class Method:
 
     @classmethod
     def params(self, method):
+        param_grid = None
         if method == "logistic":
             param_grid = [{"logistic__C": np.logspace(-4, 4, 20)}]
+        elif method == 'svm':
+            param_grid = [{'svm__C': np.logspace(-4, 4, 20)}]
+        elif method == 'naive_bayes':
+            param_grid = [{'naive_bayes__alpha':(0.1, 0.5, 1.0, 1.5, 2)}]
         return param_grid
 
     @classmethod
     def logistic(self):
         return LogisticRegression()
 
+    @classmethod
     def svm(self):
         return SVC()
 
-    def naives_bayes(self):
+    @classmethod
+    def naive_bayes(self):
         return BernoulliNB()
 
     @classmethod
@@ -178,15 +180,72 @@ def plot_confusion_mat(y_true, y_pred, name="cm"):
     plt.savefig(name)
 
 
+def get_results(clf, x_train, x_test, y_train, y_test, clf_name='q1'):
+
+    clf.fit(x_train, y_train)
+    train_acc = clf.score(x_train, y_train)
+    test_acc = clf.score(x_test, y_test)
+    y_pred = clf.predict(x_test)
+
+    print("train acc:", train_acc)
+    print("test acc:", test_acc)
+    print("best estimator", clf.best_estimator_)
+    print("get_params", clf.best_params_)
+
+
+def baseline(x_train, x_test, y_train, y_test):
+    x_train = Preprocess.lemma(x_train)
+    x_test = Preprocess.lemma(x_test)
+   
+    pipe = Pipeline(steps=[("raw", Unigram.raw()), ("logistic", Method.dummy())])
+    pipe.fit(x_train, y_train)
+    get_results(pipe, x_train, x_test, y_train, y_test)
+
+    x_train = Preprocess.stem(x_train)
+    x_test = Preprocess.stem(x_test)
+   
+    pipe = Pipeline(steps=[("raw", Unigram.raw()), ("logistic", Method.dummy())])
+    pipe.fit(x_train, y_train)
+    get_results(pipe, x_train, x_test, y_train, y_test)
+
+
+def q1(x_train, x_test, y_train, y_test, unigram_meth=None):
+    """
+    with raw() and all the prediction methods
+    """
+    methods_to_try = ["svm"]#, "naive_bayes", "logistic", "dummy"]
+    if unigram_meth is not None:
+        unigram = unigram_meth
+    else:
+        unigram = Unigram.raw()
+    for m in methods_to_try:
+        if m == "svm":
+            pipe = Pipeline(steps=[("raw", unigram), ("svm", Method.svm())])
+            param = Method.params(m)
+        elif m == "naive_bayes":
+            pipe = Pipeline(steps=[("raw", unigram), ("naive_bayes", Method.naive_bayes())])
+            param = Method.params(m)
+        elif m == 'logistic':
+            pipe = Pipeline(steps=[("raw", unigram), ("logistic", Method.logistic())])
+            param = Method.params(m)
+        clf = GridSearchCV(pipe, param_grid=param)
+        clf.fit(x_train, y_train)
+        get_results(clf, x_train, x_test, y_train, y_test)
+
+
+
+
+
 def main():
     negative_reviews = load_to_text(NEG_FILENAME)
     positive_reviews = load_to_text(POS_FILENAME)
 
     # convert to unigram counts
     x, y = label_and_merge(positive_reviews, negative_reviews)
-    preprocess = ["na", "stem", "lemma"]
+    preprocess = ["raw", "stem", "lemma"]
     methods_to_try = ["svm", "naive_bayes", "logistic", "dummy"]
     x_train, x_test, y_train, y_test = train_test_split(x, y)
+
 
     # for p in preprocess:
     #     if preprocess == "stem":
@@ -196,19 +255,14 @@ def main():
     #         x_train = Preprocess.lemma(x_train)
     #         x_test = Preprocess.lemma(x_test)
 
-    pipe = Pipeline(steps=[("raw", Unigram.raw()), ("logistic", Method.logistic())])
 
-    clf = GridSearchCV(pipe, param_grid=Method.params("logistic"))
-
-    clf.fit(x_train, y_train)
-    train_acc = clf.score(x_train, y_train)
-    test_acc = clf.score(x_test, y_test)
-    y_pred = clf.predict(x_test)
-
-    plot_confusion_mat(y_pred, y_test)
-
-    print("best estimator", clf.best_estimator_)
-    print("get_params", clf.best_params_)
+    q1(x_train, x_test, y_train, y_test)
+    # print("experiments with raw unigrams")
+    # q1(x_train, x_test, y_train, y_test)
+    # print("unigram without infrequent words")
+    # q1(x_train, x_test, y_train, y_test, Unigram.rm_infreq_words())
+    # print("unigram remove stop words")
+    # q1(x_train, x_test, y_train, y_test, Unigram.rm_stopwords())
 
 
 if __name__ == "__main__":
