7735e7865c8ce7f901187034cb937349d6283fe0
Y. Violet Guo <yueguo598@gmail.com>
2019-10-10 19:42:04 -0400

    runs, 1% acc

--- ass2/decipher.py
+++ ass2/decipher.py
@@ -2,21 +2,36 @@ import argparse
 from nltk.tag import hmm
 import os
 import nltk
-nltk.download('treebank')
+nltk.download('brown')
+nltk.download('universal_tagset')  
+
+from nltk.corpus import brown
+from nltk import sent_tokenize, word_tokenize
+
+
 
 
 def split_list_char(src):
-	dest_char = []
-
-	# break down a list into charactors
-	for i in range(len(src)):
-		
-		dest_char.append(src[i].split())
-
-	return dest_char
-
+	dest = []
+	for sent in src:
+		temp = []
+		s_temp = sent_tokenize(sent)
+		for word in sent:
+			for c in word:
+				if c != '\n':
+					temp.append(c)
+		dest.append(temp)
+	return dest
 
 def load_files(p='a2data/cipher1/', mode='train'):
+	tagged_sentences = brown.tagged_sents(categories="news", tagset="universal")
+
+	# let's keep 20% of the data for testing, and 80 for training
+	i = int(len(tagged_sentences)*0.2)
+	train_sentences = tagged_sentences[i:]
+	test_sentences = tagged_sentences[:i]
+	print(test_sentences[0])
+
 	with open((p+mode+'_plain.txt'), 'r') as f:
 		label = f.readlines()
 
@@ -25,12 +40,17 @@ def load_files(p='a2data/cipher1/', mode='train'):
 
 	label_char = split_list_char(label)
 	data_char = split_list_char(data)
-	print(data_char[0])
+	# print(label_char[0])
 	corprus = []
 	# merge data label
-	for i in range(len(data_char)):
-		corprus.append(((data_char[i], label_char[i])))
 
+	for i in range(len(data_char)):
+		temp = []
+		for j in range(len(data_char[i])):
+			#temp.append((data_char[i], label_char[i]))
+			temp.append((data_char[i][j], label_char[i][j]))
+		corprus.append(temp)
+	print(corprus[0])
 	return corprus
 
 
@@ -40,7 +60,12 @@ def hmm_base():
 	test_corpus = load_files(mode='test')
 	trainer = hmm.HiddenMarkovModelTrainer()
 	tagger = trainer.train_supervised(train_corpus)
-	print(tagger.tag(test_corpus))
+
+	res = tagger.evaluate(test_corpus)
+
+	# accruacy
+	print(res)
+
 
 
 
